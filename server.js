87274135130933cae6f51aa6550617670f8c64e8
Leonardo Mota <leonardo.mota.17@hotmail.com>
2024-11-19 17:00:40 -0300

    Leitura de posts via ID

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 import express from "express";
 
+const posts = [
+    {
+      descricao: "Foto para teste",
+      imagem: "https://placecats.com/millie/300/150",
+      id: 1
+    },
+    {
+      descricao: "Gato fofo dormindo",
+      imagem: "https://placecats.com/millie/300/150",
+      id: 2
+    },
+    {
+        descricao: "Gato fofo chorando",
+        imagem: "https://placecats.com/millie/300/150",
+        id: 3
+      },
+  ];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000,() => {
     console.log("Servidor escutando");
 });
 
-app.get("/api", (req,res) => {
-    res.status(200).send("Boas vindas");
+app.get("/posts", (req,res) => {
+    res.status(200).json(posts);
 });
+
+function buscarPost(id){
+    return posts.findIndex((post) => {
+        return post.id == Number(id)
+    })
+}
+
+app.get("/posts/:id", (req,res) => {
+    const index = buscarPost(req.params.id)
+    res.status(200).json(posts[index]);
+});
+
